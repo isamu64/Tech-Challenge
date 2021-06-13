0aab3b1fc3a78ce2dc7155d5faaeb36332ccda5f
isamu64 <fco0810@yahoo.fr>
2021-06-13 12:20:27 +0200

    modify the structure of the database-include SERIAL to make ID automatically

--- Back/data/members.sql
+++ Back/data/members.sql
@@ -4,14 +4,14 @@ DROP TABLE IF EXISTS "members";
 
 
 CREATE TABLE IF NOT EXISTS "members" (
-    "id" INTEGER NOT NULL,
+    "id" SERIAL,
     "name" TEXT NOT NULL
 );
 
 
 --Data
 
-INSERT INTO "members" ("id", "name") VALUES
-(1, 'Eleftheria'),
-(2, 'Gennadios'),
-(3, 'Lysimachos');
+INSERT INTO "members" ("name") VALUES
+('Eleftheria'),
+('Gennadios'),
+('Lysimachos');
